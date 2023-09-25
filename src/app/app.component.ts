58505f3d335ad3a8319f5afaf3222f59425ea793
lordfalah <irfin1581@gmail.com>
2023-09-25 20:48:53 +0700

    tugas 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angularBootstrap';
+  nim = '212102390';
+  nama = 'Irfin Falah';
+  title = 'Angular';
+  showSearchBox: boolean = false;
+  inputText: string = '';
+
+  toggleSearch() {
+    this.showSearchBox = true;
+  }
+  resetSearch() {
+    this.showSearchBox = false;
+    this.inputText = ''; // Reset the input field when canceling
+  }
 }
